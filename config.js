58b2171594e65295e248ed8e6cf1946551619886
Gustavo Neves Alves <gustavo.na@gmail.com>
2012-03-08 18:09:30 -0300

    Padronizando a parametrizacao do sistema

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = {
+	oauth_action_request_token: 'http://echo.lab.madgex.com/request-token.ashx',
+	oauth_consumer_key: 'key',
+	oauth_consumer_secret: 'secret'
+};
+
+module.exports = config;
